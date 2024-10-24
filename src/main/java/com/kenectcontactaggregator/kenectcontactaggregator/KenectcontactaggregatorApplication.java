8c337ccd9dbdbbf4f13fb61a52cafa6099668d1e
Daniel Aguiar <danielramos.aguiar@gmail.com>
2024-10-24 20:39:02 -0300

    first commit

---
+++ src/main/java/com/kenectcontactaggregator/kenectcontactaggregator/KenectcontactaggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.kenectcontactaggregator.kenectcontactaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenectcontactaggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenectcontactaggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenectcontactaggregator/kenectcontactaggregator/KenectcontactaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenectcontactaggregator.kenectcontactaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenectcontactaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
